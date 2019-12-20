9131131c21cd0329e9aea6dabb23aabe8a65c7af
Krishna451 <55433293+Krishna451@users.noreply.github.com>
2019-12-20 18:06:58 +0530

    Add files via upload

---
+++ secondapp.js
@@ -0,0 +1,21 @@
+
+
+//print();
+function print(){
+
+    console.log("js on Node side....")
+}
+
+
+/* msg();
+var msg=function(){
+    console.log("Function expresiion......")
+}
+ */
+
+ //3rd way to declare a function
+
+ var print1=()=>console.log("Arrow Fnction");
+ print1();
+
+ 
